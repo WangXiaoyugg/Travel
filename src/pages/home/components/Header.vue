efad440b4fe77aeec8a4c1a5fa50e5102ab87560
GarenWang <2270099149@qq.com>
2018-04-23 10:48:16 +0800

    use iconfont, webpack alias, stylus variables

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-     <div class="header-left">返回</div>
-     <div class="header-input">输入城市/景点/游玩主题</div>
-     <div class="header-right">城市</div>
+    <div class="header-left">
+       <div class="iconfont icon-back">&#xe624;</div>
+    </div>
+    <div class="header-input">
+       <span class="iconfont">&#xe632;</span>
+       输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+       城市
+       <span class="iconfont icon-arrow">&#xe65c;</span>
+    </div>
   </div>
 </template>
 
@@ -13,15 +21,19 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '~styles/variables.styl'
 .header
   display: flex
   height: .86rem
   line-height: .86rem
-  background: #00bcd4
+  background: $bgColor
   color: #fff
   .header-left
     width: .64rem
     float: left
+    .icon-back
+      text-align: center
+      font-size: .4rem
   .header-input
     flex: 1
     background-color: #fff
@@ -30,9 +42,13 @@ export default {
     height: .64rem
     line-height: .64rem
     border-radius: .1rem
+    padding-left: .2rem
     color: #ccc
   .header-right
     width: 1.24rem
     float: right
     text-align: center
+    .icon-arrow
+      font-size: .34rem
+      margin-left : -0.08rem
 </style>
