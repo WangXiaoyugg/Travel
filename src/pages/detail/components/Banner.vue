0025da1692639110c0f967eae5657b04289f49ed
GarenWang <2270099149@qq.com>
2018-04-24 21:16:16 +0800

    detail-banner

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="banner">
+    <img src="http://img1.qunarzz.com/sight/p0/1507/36/ce3d2d6c9ab44d67ae68d940b8781829.water.jpg_600x330_f68eb231.jpg"
+         alt="banner"
+         class="banner-img">
+    <div class="banner-info">
+      <div class="banner-title">北京野生动物园(AAAA景区)</div>
+      <div class="banner-number">
+        <span class="iconfont icon-photo">&#xe6dd;</span>11
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+.banner
+  overflow: hidden
+  position: relative
+  height: 0
+  padding-bottom: 55%
+  .banner-img
+    width: 100%
+  .banner-info
+    display: flex
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    height: .6rem
+    line-height: .6rem
+    color: #fff
+    background-image: linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,.8))
+    .banner-title
+      flex: 1
+      padding: 0 .2rem
+      font-size: .3rem
+    .banner-number
+      margin-top: .14rem
+      height: .32rem
+      line-height: .32rem
+      padding: 0.02rem .4rem
+      background-color: rgba(0, 0, 0, .8)
+      border-radius: .2rem
+      text-align: center
+      font-size: .24rem
+      .icon-photo
+        font-size: .24rem
+        margin-right: .1rem
+</style>
